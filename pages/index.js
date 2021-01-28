99bd4a8ee7fe97f63a8fe21b3df483376f9dcb57
PedroHolo <holodniakpedro@gmail.com>
2021-01-28 00:15:45 -0300

    segunda aula aluraQuiz

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import styled from 'styled-components'
 import db from '../db.json'
 import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
-
+import Head from 'next/head'
+import { useRouter} from 'next/router'
 // const BackgroudeImage = styled.div `
 //   background-image: url(${db.bg});
 //   flex: 1;
@@ -24,8 +26,14 @@ export const QuizContainer = styled.div `
 `
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>  
         <Widget>
           <Widget.Header>
@@ -33,6 +41,21 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={ function(event){
+                event.preventDefault();
+                router.push(`/quiz?name=${name}`);
+            }}>
+              <input 
+                onChange={function(event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Digite aqui seu nome meu consagrado!" 
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Bora lá!
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
